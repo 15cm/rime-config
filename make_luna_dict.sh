02fb0b24a0ac351c4a0deaf5bdb4061b658fbfbe
Sinkerine <git@15cm.net>
2018-03-28 22:30:43 -0400

    [+] output source dictionary in generated dictionary

--- make_luna_dict.sh
+++ make_luna_dict.sh
@@ -3,5 +3,6 @@ luna_dict="./xhup_luna.dict.yaml"
 luna_head="./xhup_luna.dict.yaml.head"
 cat "$luna_head" > "$luna_dict"
 for f in double_pinyin_flypy.*.dict.yaml; do
+  echo "#$f" >> "$luna_dict"
   sed -n 's/\t.*//p' "$f" >> "$luna_dict"
 done
