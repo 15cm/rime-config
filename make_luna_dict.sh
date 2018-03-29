1ad2ddd38dfbb50504e051d82ddb17f307e04385
Sinkerine <git@15cm.net>
2018-03-28 23:09:00 -0400

    [!] rename sougou dictionaries with prefix "extend.sougou"

--- make_luna_dict.sh
+++ make_luna_dict.sh
@@ -2,7 +2,7 @@
 luna_dict="./xhup_luna.dict.yaml"
 luna_head="./xhup_luna.dict.yaml.head"
 cat "$luna_head" > "$luna_dict"
-for f in double_pinyin_flypy.*.dict.yaml; do
+for f in extend.sougou.*.dict.yaml; do
   echo "#$f" >> "$luna_dict"
   sed -n 's/\t.*//p' "$f" >> "$luna_dict"
 done
