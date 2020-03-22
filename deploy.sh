e039891cc7fa88ab74f133d7b3ee9aa20281d593
Sinkerine <git@15cm.net>
2020-03-21 23:53:07 -0700

    Clean up fcitx/rime/ before deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp *.yaml ~/.config/fcitx/rime
+mkdir -p ~/.config/fcitx/rime && rm ~/.config/fcitx/rime/*.yaml && cp *.yaml ~/.config/fcitx/rime
+mkdir -p ~/.config/fcitx/rime/build && cp build_data/* ~/.config/fcitx/rime/build/
