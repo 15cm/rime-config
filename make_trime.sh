ca0d4e36de81200bcad070b0e07f4cbcc8d95cf3
Sinkerine <git@15cm.net>
2020-03-21 23:51:39 -0700

    Merge trime-config

---
+++ make_trime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf rime && mkdir rime
+find . -maxdepth 1 -name '*.yaml' -not -name 'installation*.yaml' -not -name 'squirrel*.yaml' -exec cp {} rime/ \;
+cp ./essay.txt rime/
+cp installation-trime.yaml rime/installation.yaml
+cp -R build_data rime/build
+
+# Copy userdb manually before trime support synchronizing userdb
+cp -R ~/.config/fcitx/rime/*.userdb rime/
