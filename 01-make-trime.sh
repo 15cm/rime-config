7a3bf73b5fc40889c8b3b4d84b18d45f3d122506
Sinkerine <git@15cm.net>
2021-09-03 23:12:36 -0700

    Add extension fonts installation to scripts

--- 01-make-trime.sh
+++ 01-make-trime.sh
@@ -3,5 +3,12 @@ set -e
 
 sed -i 's/fcitx/trime/' rime/installation.yaml
 
-# Copy userdb manually before trime support synchronizing userdb
+# Copy userdb manually before trime support synchronizing userdb.
 cp -R ~/.local/share/fcitx5/rime/*.userdb rime/
+
+# External fonts.
+mkdir -p fonts
+wget iij.dl.sourceforge.jp/hanazono-font/62072/hanazono-20141012.zip -O HanaMin.zip \
+  && unzip HanaMin.zip -d HanaMin \
+  && mv HanaMin/HanaMinB.ttf fonts/ \
+  && rm -r HanaMin.zip HanaMin
